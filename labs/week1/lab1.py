9589c95357415d649ada0037742d09315c07b35b
Henil5204 <101511850@georgebrown.ca>
2025-01-19 15:40:59 -0500

    updated

---
+++ labs/week1/lab1.py
@@ -0,0 +1,23 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Henil
+# Last Name: Patel
+
+#
+array = [1, 4, 7, 9]
+
+
+a, b, c, d = 1, 2, 3, 4
+
+
+
+
+# Fully bracketed version of e = a - b ** c // d + a % c
+e = ((a - ((b ** c) // d)) + (a % c))
+
+# Formatting
+temperature = 32.6
+print(f"The Today temperature is : {temperature :.3f} degrees Celsius")
+
+# Common Functions
+userAge = int(input("Enter your age: ")) + 22
+print(f"Now showing the shop items filtered by age: {userAge}")
